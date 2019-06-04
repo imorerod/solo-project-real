2ca145ad62597f5c538d6f6a9f4e2e5771d9b678
Isaac Morerod <isaacmorerod@isaacsMacBookPro.local>
2019-06-04 15:04:33 -0500

    parent added to db but not being recognized at login

--- database.sql
+++ database.sql
@@ -1,30 +1,32 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-    "clearance_level" INTEGER NOT NULL DEFAULT 0
+CREATE TABLE "parent" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
 );
 
-INSERT INTO "user" ("username", "password", "clearance_level")
-VALUES ('Admiral Greer', 'tuna', 18),
-('Captain Borodin', 'shark', 10),
-('Lieutenant Nguyen', 'fishy', 4),
-('Lieutenant Ryan', 'tuna', 4);
-
-
-CREATE TABLE "secret" (
-    "id" SERIAL PRIMARY KEY,
-    "content" VARCHAR (80) UNIQUE NOT NULL,
-    "secrecy_level" INTEGER NOT NULL DEFAULT 0
+CREATE TABLE "child" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80) UNIQUE NOT NULL,
+	"number" VARCHAR (12) UNIQUE NOT NULL,
+	"approved_id" INTEGER NOT NULL
 );
 
-INSERT INTO "secret" ("content", "secrecy_level")
-VALUES ('Admirals Only: Captain Borodin is totally weird.', 13),
-('Captains Or Above: Lieutenant Ryan is looking fly.', 6),
-('Lieutenants Or Above: We are heading to the Bahamas.', 3);
+CREATE TABLE "contacts" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80) UNIQUE NOT NULL,
+	"number" VARCHAR (12) UNIQUE NOT NULL
+);
 
+CREATE TABLE "approved" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80) UNIQUE NOT NULL,
+	"number" VARCHAR (12) UNIQUE NOT NULL
+);
+
+CREATE TABLE "non_approved" (
+	"id" SERIAL PRIMARY KEY,
+	"number" VARCHAR (12) UNIQUE NOT NULL,
+	"time" TIMESTAMP NOT NULL,
+	"child_id" INTEGER NOT NULL,
+	"pending" BOOLEAN DEFAULT FALSE
+);
--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/secrets', secretsRouter);
 
 // Serve static files
 app.use(express.static('build'));
